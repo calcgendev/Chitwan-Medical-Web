b5623ed895ece4f36dc4b1bfda9197e4aa7ee2d7
saroj <saroj1229@xavier.edu.np>
2025-12-30 11:24:44 +0545

    minor chnages made in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,10 @@ import heroDoctor from "@/assets/image.jpg";
 
 const Hero = () => {
   return (
-    <section className="relative bg-gradient-to-br from-background via-background to-secondary pt-8 md:pt-16 pb-6 overflow-hidden min-h-[280px] md:min-h-[520px]" id="home">
+    <section
+      className="relative bg-gradient-to-br from-background via-background to-secondary pt-8 md:pt-16 pb-6 overflow-hidden min-h-[280px] md:min-h-[520px]"
+      id="home"
+    >
       {/* background image with fixed size */}
       <div
         className="absolute top-0 left-1/2 transform -translate-x-1/2 z-0"
@@ -18,16 +21,24 @@ const Hero = () => {
       </div>
 
       <div className="container mx-auto px-0 relative">
-        <div className="grid grid-cols-3 md:grid-cols-2 gap-8 items-center" style={{ minHeight: 'min(35vw, 520px)' }}>
+        <div
+          className="grid grid-cols-3 md:grid-cols-2 gap-8 items-center"
+          style={{ minHeight: "min(35vw, 520px)" }}
+        >
           <div className="space-y-4 z-20 relative max-w-xl col-span-2 md:col-span-1 mx-auto md:mx-0 px-3 md:px-0 translate-x-2 md:translate-x-0">
-            <p className="text-accent text-sm font-semibold tracking-wider uppercase">Caring for life</p>
+            <p className="text-accent text-sm font-semibold tracking-wider uppercase">
+              Caring for life
+            </p>
             <h1 className="text-2xl sm:text-4xl md:text-6xl font-bold leading-tight text-foreground">
-              Leading the Way in Medical Excellence
+              Caring for Every Life, Every Day
             </h1>
             <a href="/services">
-            <Button size="lg" className="bg-[#BFD2F8] text-black shadow-lg px-4 py-5 rounded-md">
-              Our Services
-            </Button>
+              <Button
+                size="lg"
+                className="bg-[#BFD2F8] text-black shadow-lg px-4 py-5 rounded-md mt-5"
+              >
+                Our Services
+              </Button>
             </a>
           </div>
           <div className="col-span-1" />
